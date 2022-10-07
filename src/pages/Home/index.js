187a9708b0b5f0eaf99943e131d1e3b27b22c699
Nathalia <nathalia.gramst@gmail.com>
2022-10-06 23:28:05 -0300

    Text under the book image + carosel arrows

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,21 @@
 import Footer from "../../components/Footer";
-import { Wrapper } from "./styles";
+import { Wrapper, BookCarousel } from "./styles";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export default function Home() {
     return (
         <>
             <Wrapper>
                 <header></header>
-                <div className="image-container">
-                    <img src="https://m.media-amazon.com/images/I/81pA6-hv+2L.jpg" alt="" />
-                </div>
+                <BookCarousel>
+                    <div>
+                        <IoIosArrowBack />
+                        <img src="https://m.media-amazon.com/images/I/81pA6-hv+2L.jpg" alt="" />
+                        <IoIosArrowForward />
+                    </div>
+                    <h2>The Institute</h2>
+                    <h3>By Stephen King</h3>
+                </BookCarousel>
                 <div className="main"></div>
             </Wrapper>
             <Footer />
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,26 +3,10 @@ import styled from "styled-components";
 const Wrapper = styled.main`
     background-color: #407088;
     width: 100vw;
-    height: 100vh;
+    height: auto;
 
     header {
         height: 30vh;
-    }
-
-    .image-container {
-        width: 100%;
-        height: auto;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        top: 10vh;
-    }
-
-    img {
-        object-fit: cover;
-        width: 275px;
-        box-shadow: 0px 0px 10px -4px rgba(0, 0, 0, 0.25);
     }
 
     .main {
@@ -33,6 +17,48 @@ const Wrapper = styled.main`
     }
 `;
 
+const BookCarousel = styled.section`
+    width: 100%;
+    height: auto;
+    position: absolute;
+    top: 10vh;
+
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    img {
+        object-fit: cover;
+        width: 275px;
+        box-shadow: 0px 0px 10px -4px rgba(0, 0, 0, 0.25);
+    }
+
+    svg {
+        font-size: 30px;
+        color: #70ADB5;
+    }
+
+    h2, h3 {
+        color: #333333;
+        text-align: center;
+    }
+
+    h2 {
+        font-family: 'Libre Baskerville', serif;
+        font-size: 30px;
+        margin-top: 1rem;
+    }
+
+    h3 {
+        font-family: 'Roboto', sans-serif;
+        font-size: 13px;
+        margin-top: .2rem;
+    }
+`;
+
 export {
-    Wrapper
+    Wrapper,
+    BookCarousel,
 };
